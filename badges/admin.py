fc6390d676a0b36f6878d6781a60dd71c13d9f59
reza rezaee <r871017h800501@gmail.com>
2022-06-05 00:49:44 +0430

    improved admin panel for skills app

--- badges/admin.py
+++ badges/admin.py
@@ -4,17 +4,21 @@ from .models import Skill,Experience,InterestGroup,InterestBadge
 @admin.register(Skill)
 class AdminSkills(admin.ModelAdmin):
     list_display = ['id','name','is_active', 'is_familiarity']
+    list_display_links = list_display
 
 @admin.register(InterestGroup)
 class AdminInterestGroup(admin.ModelAdmin):
     list_display = ['id','name','is_active' ]
+    list_display_links = list_display
 
 @admin.register(InterestBadge)
 class AdminInterestBadge(admin.ModelAdmin):
     list_display = ['id','name','is_active', 'group']
+    list_display_links = list_display
 
 
 @admin.register(Experience)
 class AdminExperience(admin.ModelAdmin):
     list_display = ['id','position','company', 'location', 'starting_date','ending_date', ]
+    list_display_links = list_display
 
